7a8f82e28bf975703be51932724ed59ccd92e650
strivemag <talga.tawit@gmail.com>
2017-05-08 07:51:05 +0600

    Styling autocomplete search

--- src/components/SearchUserListItem.js
+++ src/components/SearchUserListItem.js
@@ -5,7 +5,8 @@ class SearchUserListItem extends React.Component {
     return (
       <li>
         <a href={`/${this.props.user.username}`}>
-          <img width="35" className="avatar-image" src={getAvatarUrl(this.props.user.avatarUrl)} alt={`${this.props.user.username} avatar`} />
+          <div className="avatar-image" style={{backgroundImage: `url(${getAvatarUrl(avatarUrl)})`, backgroundSize: 'cover'}}></div>
+          {/* <img width="35" className="avatar-image" src={getAvatarUrl(this.props.user.avatarUrl)} alt={`${this.props.user.username} avatar`} /> */}
           <span dangerouslySetInnerHTML={{ __html: this.props.user.username }} />
         </a>
       </li>
