093ef508c5325baf4098dc8cd2e0d98984bcea3d
strivemag <talga.tawit@gmail.com>
2017-05-09 17:21:58 +0600

    Adding image to AutoCompleteSearch

--- src/components/SearchUserListItem.js
+++ src/components/SearchUserListItem.js
@@ -5,9 +5,9 @@ class SearchUserListItem extends React.Component {
     return (
       <li>
         <a href={`/${this.props.user.username}`}>
-          <div className="avatar-image" style={{backgroundImage: `url(${getAvatarUrl(this.props.user.avatarUrl)})`, backgroundSize: 'cover'}}>
-            {/* <img width="35" className="avatar-image" src={getAvatarUrl(this.props.user.avatarUrl)} alt={`${this.props.user.username} avatar`} /> */}
-          </div>
+          {/* <div className="avatar-image" style={{backgroundImage: `url(${getAvatarUrl(this.props.user.avatarUrl)})`, backgroundSize: 'cover'}}> */}
+            <img width="35" className="avatar-image" src={getAvatarUrl(this.props.user.avatarUrl)} alt={`${this.props.user.username} avatar`} />
+          {/* </div> */}
           <span dangerouslySetInnerHTML={{ __html: this.props.user.username }} />
         </a>
       </li>
